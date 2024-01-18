f0c57de5ae064ae8ac37241a0f2165f11bf8b91f
Yana Shcherbyna <101975159+Yana-Shcherbyna@users.noreply.github.com>
2024-01-18 13:06:06 +0200

    Update articles.php

--- views/articles.php
+++ views/articles.php
@@ -26,7 +26,7 @@
 
     </div>
     <footer>
-      <p>My first blog <br> Copyright &copy; 2023</p>
+      <p>My first blog <br> Copyright &copy; <?php echo date("Y"); ?></p>
     </footer>
   </div>
 </body>
